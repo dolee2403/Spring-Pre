70732bbdabddba8015ae0fd348d6698dbb7be5ef
이도 <skydoya191539@naver.com>
2024-05-17 15:10:06 +0900

    init commit

---
+++ src/main/java/com/sparta/springpre/SpringPreApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springpre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPreApplication.class, args);
+    }
+
+}
